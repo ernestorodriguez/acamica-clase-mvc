ef8fc13d2f45e18b98068d1786e3224f8ff14f37
Ernestrod <ernesto@circuloconcentrico.com>
2020-01-22 18:14:57 -0300

    added basic solution

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 function init() {
-  const model = {
-    listaTareas: new ListaTareas([new Tarea("primera"), new Tarea("second")])
-  };
-  const controller = new Controller(model);
-  console.log({ controller, model });
+  const $modal = $('[data-js="modal"]');
+  const $closeModalButton = $('[data-js="close-modal"]');
+  const $addButton = $('[data-js="add-btn"]');
+  const $input = $('[data-js="task-input"]');
+  const $list = $('[data-js="task-list"]');
+  const $quantity = $('[data-js="quantity"');
+
+  $addButton.click(() => {
+    const value = $input.val();
+    if(value) {
+      $list.append(`<li>${$input.val()}</li>`);
+      $input.val('');
+    }
+    $modal.fadeIn(300);
+    $quantity.text( $list.children().length);
+  });
+
+  $closeModalButton.click(() => {
+    $modal.fadeOut(300);
+  });
 }
 
 window.onload = init;
